8c6c2d83ef9f4dba91499880c572ff78d6c55ad9
ElhaddajAya <ayaelhaddaj619@gmail.com>
2025-03-30 00:49:04 +0100

    First commit.

---
+++ weather_app_2App.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_2App.swift
+//  weather-app-2
+//
+//  Created by aya on 29/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct weather_app_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
